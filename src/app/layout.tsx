6b164f8bf0e78649a508921b4788ace1604c3521
Milena Sol Aron <aronmilena.it@gmail.com>
2024-11-12 08:09:46 -0300

    refactor: add more font weights.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,9 @@ import { Lato } from "next/font/google";
 import "./globals.css";
 
 const lato = Lato({
-  weight: '400',
-  subsets: ['latin'],
-})
+  weight: ["100", "300", "400", "700", "900"],
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Milena Sol Aron",
@@ -19,11 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${lato.className} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${lato.className} antialiased`}>{children}</body>
     </html>
   );
 }
