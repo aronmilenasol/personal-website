be78b4817a262d3ce2390570564f90f5d7951db9
Milena Sol Aron <aronmilena.it@gmail.com>
2024-11-21 10:11:40 -0300

    fix: delete unnecesary layout and page and use locale as type any.

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,24 +2,30 @@ import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 import { notFound } from "next/navigation";
 import { routing } from "@/i18n/routing";
+import type { Metadata } from "next";
+import "./globals.css";
 
-export default async function LocaleLayout({
+export const metadata: Metadata = {
+  title: "Milena Sol Aron",
+  description: "My personal website and portfolio as a Software Developer.",
+};
+
+export default async function Layout({
   children,
-  params,
+  params: { locale },
 }: {
   children: React.ReactNode;
-  params: Promise<{ locale: string }>;
+  params: { locale: string };
 }) {
-  const { locale } = await params;
-
-  if (!routing.locales.includes(locale as "en" | "es")) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  if (!routing.locales.includes(locale as any)) {
     notFound();
   }
 
   const messages = await getMessages();
 
   return (
-    <html lang={locale}>
+    <html lang="en">
       <body>
         <NextIntlClientProvider messages={messages}>
           {children}
