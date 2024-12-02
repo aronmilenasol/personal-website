52b4deeabde0708d0b463a7fddf151e8c03459e9
Milena Sol Aron <aronmilena.it@gmail.com>
2024-12-02 19:37:20 -0300

    refactor: remove unnecessary const and imports.

--- src/app/[locale]/resume/page.tsx
+++ src/app/[locale]/resume/page.tsx
@@ -1,5 +1,4 @@
 import { useTranslations } from "next-intl";
-import { Star, Sparkles } from "lucide-react";
 import ResumeAbout from "../components/ResumeAbout";
 import ResumeExperience from "../components/ResumeExperience";
 import ResumeEducation from "../components/ResumeEducation";
@@ -8,15 +7,6 @@ import ResumeLanguages from "../components/ResumeLanguages";
 
 export default function Resume() {
   const t = useTranslations("Resume");
-  const paragraphClasses =
-    "text-base sm:text-lg md:text-xl leading-relaxed text-pink-800";
-  const titleClasses =
-    "font-bold text-2xl sm:text-3xl md:text-4xl mb-4 text-pink-600 flex items-center";
-  const jobTitleClasses = "font-bold text-xl sm:text-2xl text-pink-500 mt-4";
-  const jobRoleClasses = "font-bold text-lg sm:text-xl text-pink-500 mb-2";
-  const timelineClasses = "italic text-base sm:text-lg text-pink-500";
-  const responsibilityClasses =
-    "font-medium text-base sm:text-lg mt-2 text-pink-700";
 
   return (
     <section className="bg-gradient-to-r from-orange-50 to-pink-100 font-lato flex flex-col justify-center items-center p-4 sm:p-8 text-pink-900 min-h-screen">
