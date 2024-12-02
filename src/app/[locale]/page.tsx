a1e292d4222f48120f5a477ef3d2abd5e6584204
Milena Sol Aron <aronmilena.it@gmail.com>
2024-12-02 12:34:40 -0300

    refactor: change the homepage's design.

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -4,22 +4,22 @@ export default function Home() {
   const t = useTranslations("Homepage");
 
   return (
-    <section className="font-lato flex flex-col justify-center items-center p-4 min-h-screen bg-gradient-to-r from-orange-50 to-pink-100 text-pink-800 font-bold text-center">
-      <h1 className="text-5xl md:text-7xl font-bold mb-4 text-pink-800">
-        {t("welcome")}
-      </h1>
-      <h2 className="text-4xl md:text-6xl mb-6 text-pink-700 font-semibold pb-2">
-        {t("name")}
-      </h2>
-      <p className="text-xl md:text-2xl max-w-3xl bg-white bg-opacity-80 p-8 rounded-2xl shadow-lg border-4 border-pink-400 leading-relaxed text-pink-800 relative transform hover:scale-105 transition-transform duration-300 ease-in-out">
-        <span className="absolute top-0 left-0 -mt-4 -ml-4 text-pink-500 text-4xl">
-          🌸
-        </span>
-        <span className="absolute bottom-0 right-0 -mb-4 -mr-4 text-pink-500 text-4xl">
-          🌸
-        </span>
-        {t("description")}
-      </p>
+    <section className="flex flex-col justify-center items-center p-4 min-h-screen bg-gradient-to-r from-pink-100 to-pink-200 text-pink-800 text-center">
+      <div className="max-w-4xl w-full">
+        <h1 className="text-5xl md:text-7xl font-bold mb-4 text-pink-700 relative">
+          {t("welcome")}
+        </h1>
+        <h2 className="text-4xl md:text-6xl mb-8 text-pink-600 font-semibold">
+          {t("name")}
+        </h2>
+        <div className="bg-white bg-opacity-90 p-8 rounded-3xl shadow-lg border-2 border-pink-300 relative overflow-hidden">
+          <div className="absolute top-0 left-0 w-full h-1 bg-gradient-to-r from-pink-300 to-pink-400"></div>
+          <p className="text-xl md:text-2xl leading-relaxed text-pink-800 relative z-10">
+            {t("description")}
+          </p>
+        </div>
+      </div>
     </section>
   );
 }
+
