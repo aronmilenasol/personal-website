73189ce56c2afc36d01efe1a4d29a4449488e044
Milena Sol Aron <aronmilena.it@gmail.com>
2024-12-05 16:49:13 -0300

    refactor: simplify the design of the page.

--- src/app/[locale]/gallery/page.tsx
+++ src/app/[locale]/gallery/page.tsx
@@ -14,15 +14,10 @@ export default function Gallery() {
 
   return (
     <section className="min-h-screen bg-gradient-to-r from-pink-100 to-pink-200 text-pink-800 flex items-center justify-center flex-col p-4 md:p-8">
+      <h1 className="text-4xl md:text-5xl font-bold mb-6 text-pink-700 leading-tight text-center">
+        {t("gallery-title")}
+      </h1>
       <div className="max-w-5xl w-full bg-white rounded-3xl shadow-2xl overflow-hidden">
-        <div className="p-8 md:p-12">
-          <h1 className="text-4xl md:text-5xl font-bold mb-4 text-pink-700 leading-tight text-center">
-            {t("gallery-title")}
-          </h1>
-          <p className="text-center text-2xl md:text-2xl text-pink-600 mt-2">
-            {t("gallery-description")}
-          </p>
-        </div>
         <ImageGallery images={galleryImages} />
       </div>
     </section>
