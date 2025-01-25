9c3cc5aaddc5c1da0a8c831efb7fa7826d0f3388
Milena Sol Aron <aronmilena.it@gmail.com>
2025-01-25 11:16:08 -0300

    refactor: update currently and its components

--- src/app/[locale]/components/currently/NowReading.tsx
+++ src/app/[locale]/components/currently/NowReading.tsx
@@ -5,18 +5,18 @@ const NowReading = () => {
   const t = useTranslations("Now");
 
   return (
-    <section className="bg-white p-6 sm:p-8 rounded-3xl shadow-lg border-2 border-pink-200 transition-all duration-300 hover:shadow-xl hover:border-pink-300">
+    <section className="bg-white p-6 text-dark sm:p-8 rounded-3xl shadow-lg transition-all duration-300 hover:shadow-xl hover:border-pink-300">
       <div className="flex items-center mb-6">
-        <BookOpen className="text-pink-500 w-8 h-8 sm:w-10 sm:h-10 mr-4" />
-        <h2 className="font-bold text-2xl sm:text-3xl md:text-4xl text-pink-600">
+        <BookOpen className="w-8 h-8 sm:w-10 sm:h-10 mr-4" />
+        <h2 className="font-bold text-2xl sm:text-3xl md:text-4xl text-darker">
           {t("now-reading-title")}
         </h2>
       </div>
-      <div className="bg-pink-100 p-4 sm:p-6 rounded-2xl">
-        <p className="font-semibold text-xl sm:text-2xl md:text-3xl mb-2 text-pink-700">
+      <div className="bg-lighter shadow-md border-2 border-base p-4 sm:p-6 rounded-2xl">
+        <p className="font-semibold text-xl sm:text-2xl md:text-3xl mb-2">
           {t("book1-title")}
         </p>
-        <p className="text-base sm:text-lg md:text-xl text-pink-800 italic">
+        <p className="text-base sm:text-lg md:text-xl italic">
           {t("book1-author")}
         </p>
       </div>
