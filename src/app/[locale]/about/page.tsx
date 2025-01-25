0bad7b073a103b257bb17a90d01ce7ad809432a4
Milena Sol Aron <aronmilena.it@gmail.com>
2025-01-25 11:14:44 -0300

    refactor: update about and its components

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -4,14 +4,13 @@ import AboutLikes from "../components/about/AboutLikes";
 
 export default function About() {
   const t = useTranslations("About");
-  const paragraphClasses =
-    "text-left text-lg md:text-xl leading-relaxed mb-4 text-pink-800";
+  const paragraphClasses = "text-left text-lg md:text-xl leading-relaxed mb-4";
 
   return (
-    <section className="min-h-screen bg-gradient-to-r from-pink-100 to-pink-200 text-pink-800 flex items-center justify-center flex-col p-4 md:p-8">
+    <section className="min-h-screen bg-light text-dark flex items-center justify-center flex-col p-4 md:p-8">
       <div className="max-w-5xl w-full bg-white rounded-3xl shadow-2xl overflow-hidden">
         <div className="p-8 md:p-12">
-          <h1 className="text-4xl md:text-5xl font-bold mb-6 text-pink-700 leading-tight text-center">
+          <h1 className="text-4xl md:text-5xl font-bold mb-6 leading-tight text-center">
             {t("about-title")}
           </h1>
           <div className="text-center space-y-4">
--- src/app/[locale]/components/about/AboutWebsite.tsx
+++ src/app/[locale]/components/about/AboutWebsite.tsx
@@ -1,14 +1,14 @@
 import { useTranslations } from "next-intl";
 
 const technologiesClasses =
-  "text-center text-lg md:text-xl leading-relaxed mb-4 text-pink-800";
+  "text-center text-lg md:text-xl leading-relaxed mb-4";
 
 const AboutWebsite = () => {
   const t = useTranslations("About");
   return (
-    <div className="mt-6 md:mt-8 max-w-5xl w-full bg-white rounded-3xl shadow-2xl overflow-hidden">
+    <div className="text-dark mt-6 md:mt-8 max-w-5xl w-full bg-white rounded-3xl shadow-2xl overflow-hidden">
       <div className="p-8 md:p-12">
-        <h2 className="text-4xl md:text-5xl font-bold mb-6 text-pink-700 leading-tight text-center">
+        <h2 className="text-4xl md:text-5xl font-bold mb-6 text-darker leading-tight text-center">
           {t("about-website-title")}
         </h2>
         <div className="text-center space-y-4">
@@ -17,7 +17,7 @@ const AboutWebsite = () => {
           </p>
           <div className="mt-8 flex flex-col items-center">
             <a
-              className="text-center bg-pink-600 hover:bg-pink-700 text-white font-semibold py-3 px-6 rounded-full shadow-md transition-colors duration-300 w-full md:w-auto"
+              className="text-center bg-dark hover:bg-darker text-white font-semibold py-3 px-6 rounded-full shadow-md transition-colors duration-300 w-full md:w-auto"
               href="https://github.com/aronmilenait/personal-website"
               target="_blank"
               rel="noopener noreferrer"
