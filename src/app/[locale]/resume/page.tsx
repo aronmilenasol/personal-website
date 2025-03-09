ad01cbeb4ce11256cd096a3787c1f310460ca702
Milena Sol Aron <aronmilena.it@gmail.com>
2025-03-09 11:16:45 -0300

    refactor: remove about component from resume

--- src/app/[locale]/resume/page.tsx
+++ src/app/[locale]/resume/page.tsx
@@ -1,5 +1,4 @@
 import { useTranslations } from "next-intl";
-import ResumeAbout from "../components/resume/ResumeAbout";
 import ResumeExperience from "../components/resume/ResumeExperience";
 import ResumeEducation from "../components/resume/ResumeEducation";
 import ResumeLanguages from "../components/resume/ResumeLanguages";
@@ -12,8 +11,7 @@ export default function Resume() {
       <h1 className="text-darker text-4xl md:text-5xl font-bold mb-12 text-heading leading-tight text-center">
         {t("resume-title")}
       </h1>
-      <div className="bg-white space-y-6 sm:space-y-8 w-full max-w-4xl mx-auto bg-card p-4 sm:p-8 md:p-10 rounded-3xl shadow-2xl ">
-        <ResumeAbout />
+      <div className="bg-white space-y-6 sm:space-y-8 w-full max-w-4xl mx-auto bg-card p-4 sm:p-8 md:p-10 rounded-3xl shadow-2xl mb-8">
         <ResumeExperience />
         <ResumeEducation />
         <ResumeLanguages />
