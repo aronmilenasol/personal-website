99f7e457be071c223b1552968b25ec987e4e018f
Milena Sol Aron <aronmilena.it@gmail.com>
2025-05-31 20:36:39 -0300

    chore: update education

--- src/app/[locale]/components/resume/ResumeEducation.tsx
+++ src/app/[locale]/components/resume/ResumeEducation.tsx
@@ -1,12 +1,12 @@
 import { useTranslations } from "next-intl";
 
 const paragraphClasses =
-  "text-base sm:text-lg md:text-xl leading-relaxed text-darker";
+  "text-base sm:text-lg md:text-xl leading-relaxed text-darker mb-4";
 const titleClasses =
   "font-bold text-2xl sm:text-3xl md:text-4xl mb-4 text-darker flex items-center";
 const educationAreaClasses = "font-bold text-lg sm:text-xl text-darker mb-2";
 const institutionClasses = "font-bold text-2xl sm:text-2xl text-darker mt-4";
-const timelineClasses = "italic text-base sm:text-lg text-darker";
+const timelineClasses = "italic text-base sm:text-lg text-darker mb-2";
 
 const ResumeEducation = () => {
   const t = useTranslations("Resume");
@@ -14,8 +14,9 @@ const ResumeEducation = () => {
     <div className="mt-6 sm:mt-10">
       <h3 className={titleClasses}>{t("education-title")}</h3>
       <div className="bg-lighter rounded-2xl mb-4 sm:mb-6">
-        {/* UTN */}
+        {/* University of the People */}
         <p className={institutionClasses}>{t("education1-institution")}</p>
+        <p className={paragraphClasses}>{t("education1-country")}</p>
         <p className={educationAreaClasses}>{t("education1-area")}</p>
         <p className={timelineClasses}>{t("education1-timeline")}</p>
         <p className={paragraphClasses}>{t("education1-description")}</p>
