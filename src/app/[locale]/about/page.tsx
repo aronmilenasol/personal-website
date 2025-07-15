5a63bc64a7ce578f90905f2d46e56df8a1ee4d62
Milena Sol Aron <aronmilena.it@gmail.com>
2025-07-15 07:47:57 -0300

    refactor: remove likes

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,13 +1,11 @@
 import AboutMe from "../components/about/AboutMe";
 import AboutWebsite from "../components/about/AboutWebsite";
-import AboutLikes from "../components/about/AboutLikes";
 
 export default function About() {
   return (
     <>
       <div className="min-h-screen bg-light text-dark flex items-center justify-center flex-col p-4 md:p-8">
         <AboutMe />
-        <AboutLikes />
         <AboutWebsite />
       </div>
     </>
